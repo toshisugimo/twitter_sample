61ef54e273ed86430791c6360ca52d8d65d66aa3
toshisugimo <toshihiro0412@gmail.com>
2017-07-13 01:48:52 +0900

    ユーザー新規登録機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'signup' => 'users#new'
+  get 'users/:id/edit' => 'users#edit'
+  post 'users/:id/update' => 'users#update'
+  post 'users/create' => 'users#create'
+  get 'users/index' => 'users#index'
+  get 'users/:id' => 'users#show'
+
   get 'posts/index' => 'posts#index'
   get 'posts/new' => 'posts#new'
   get 'posts/:id' => 'posts#show'
@@ -6,7 +13,7 @@ Rails.application.routes.draw do
   get 'posts/:id/edit' => 'posts#edit'
   post 'posts/:id/update' => 'posts#update'
   post 'posts/:id/destroy' => 'posts#destroy'
-  
+
   get '/' => 'home#top'
   get 'about' => 'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
