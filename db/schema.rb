e85dc8da3a2b7b2297f75c200dad41449b8d6d5f
toshisugimo <toshihiro0412@gmail.com>
2017-07-13 23:21:09 +0900

    ユーザー画像登録機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170712150321) do
+ActiveRecord::Schema.define(version: 20170713134008) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170712150321) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image_name"
   end
 
 end
